dbc239f3b9c312f50c7cea53951b076b77990caf
Agustín <amarquardt@movizen.com>
2023-09-05 23:19:05 -0300

    Ajustes en modelos ciudad y provincia

--- frontend/src/app/@models/ciudad.model.ts
+++ frontend/src/app/@models/ciudad.model.ts
@@ -1,6 +1,5 @@
 export interface Ciudad {
   id: number;
   descripcion: string;
-  id_provincia: number; // Esta propiedad representa la relación con Provincia
-  // Puedes agregar más propiedades según sea necesario
+  id_provincia: number;
 }
--- frontend/src/app/@models/provincia.model.ts
+++ frontend/src/app/@models/provincia.model.ts
@@ -1,5 +1,4 @@
 export interface Provincia {
   id: number;
   descripcion: string;
-  // Puedes agregar más propiedades según sea necesario
 }
